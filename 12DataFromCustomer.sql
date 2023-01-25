9e6ecc8d3b28110fce36d5b7c48c6e77fff0d476
SandraRathje <122870458+SandraRathje@users.noreply.github.com>
2023-01-25 07:44:40 +0100

    Add files via upload

---
+++ 12DataFromCustomer.sql
@@ -0,0 +1,7 @@
+﻿SELECT
+    kunde.*, bestellung.BESTELLNR,bestellung.BESTELLDATUM, bestellung.RECHNUNGSBETRAG
+FROM
+    kunde
+INNER JOIN bestellung ON kunde.KUNDENNR = bestellung.KUNDENNR
+WHERE
+    kunde.KUNDENNR = '2007';
